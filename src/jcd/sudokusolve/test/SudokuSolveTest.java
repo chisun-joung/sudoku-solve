2780381d6b5da9ff7f5a7508a1ea96d27ac8007a
chisun-joung <csjoung@gmail.com>
2013-09-28 15:49:42 +0900

    make SudokuSolveTest

---
+++ src/jcd/sudokusolve/test/SudokuSolveTest.java
@@ -0,0 +1,14 @@
+package jcd.sudokusolve.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SudokuSolveTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
